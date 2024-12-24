30948a71bd55b04408ab660f099229d58ebf7889
Franm99 <fran.moreno.se@gmail.com>
2024-12-24 07:17:44 +0100

    problemset[Week 4] - Volume

--- week4/problemset/volume/volume.c
+++ week4/problemset/volume/volume.c
@@ -17,14 +17,14 @@ int main(int argc, char *argv[])
     }
 
     // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "r");
+    FILE *input = fopen(argv[1], "rb");
     if (input == NULL)
     {
         printf("Could not open file.\n");
         return 1;
     }
 
-    FILE *output = fopen(argv[2], "w");
+    FILE *output = fopen(argv[2], "wb");
     if (output == NULL)
     {
         printf("Could not open file.\n");
@@ -34,10 +34,19 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    uint8_t header[44];
+    fread(&header, HEADER_SIZE, 1, input);
+    fwrite(&header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    while (fread(&buffer, sizeof(int16_t), 1, input) != 0)
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
 
-    // Close files
+    // Close filess
     fclose(input);
     fclose(output);
 }
