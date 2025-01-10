4f358c5ddd14b5a4d5848d07f331a2a308cef827
Franm99 <fran.moreno.se@gmail.com>
2025-01-10 07:50:47 +0100

    problemset[Week 4] - filter (not finished)

--- week4/problemset/filter/helpers.c
+++ week4/problemset/filter/helpers.c
@@ -1,21 +1,69 @@
 #include "helpers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    printf("I am here\n");
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            int avg = (image[y][x].rgbtBlue + image[y][x].rgbtGreen + image[y][x].rgbtRed) / 3;
+            image[y][x].rgbtBlue = avg;
+            image[y][x].rgbtGreen = avg;
+            image[y][x].rgbtRed = avg;
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // TODO: Add fix for images with odd width.
+    for (int x = 0; x < width/2; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            RGBTRIPLE tmp = image[y][width-x];
+            image[y][width-x] = image[y][x];
+            image[y][x] = tmp;
+
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE(*im_copy)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+    im_copy = image;
+    for (int x = 1; x < width-1; x++)
+    {
+        for (int y = 1; y < height-1; y++)
+        {
+            int valB = 0, valG = 0, valR = 0;
+            int num_elements = 0;
+            for (int i = y - 1; i <= y + 1; i++)
+            {
+                for (int j = x-1; j <= x + 1; j++)
+                {
+                    valB += im_copy[i][j].rgbtBlue;
+                    valG += im_copy[i][j].rgbtGreen;
+                    valR += im_copy[i][j].rgbtRed;
+
+                    num_elements++;
+                }
+            }
+
+            image[y][x].rgbtBlue = valB / num_elements;
+            image[y][x].rgbtGreen = valG / num_elements;
+            image[y][x].rgbtRed = valR / num_elements;
+        }
+    }
     return;
 }
 
