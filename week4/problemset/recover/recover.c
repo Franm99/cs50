c1bd7dc936acadccf904d2d1c19a37ef63de0b64
Franm99 <fran.moreno.se@gmail.com>
2025-01-14 07:23:24 +0100

    problemset[week 4] - recover

--- week4/problemset/recover/recover.c
+++ week4/problemset/recover/recover.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
  
 typedef uint8_t  BYTE;
 
@@ -14,16 +15,55 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    BYTE init_bytes = 0xff;
     int block_size = 512;
 
-    BYTE b;
+    BYTE b[512];
     int i = 0;
-    while (fread(&b, sizeof(b) * block_size, 1, f) != 0)
+    int im_count = 0;
+    bool in_image = false;
+
+    /**
+     * Loop in 512 blocks:
+     * - starts with the set of bytes wanted?
+     *  If yes:
+     *      - was the flag raised?
+     *      If No:
+     *          - New image starts. Start saving into a new file. Save the current memory block.
+     *      If Yes:
+     *          - End of previous image. Close its file. Open a new one and save this new block.
+     *  If not:
+     *      - is the flag raised?
+     *      If No:
+     *          - No image. Jump to next memory block.
+     *      If Yes:
+     *          - Still in image. Keep saving to current memory block.
+     */
+    FILE *imptr = NULL;
+    while (fread(&b, sizeof(b), 1, f) != 0)
     {
         // Check jpeg header
-        printf("im %d\n", i);
-        i++;
+        // printf("mem block %d\n", i);
+        // i++;
+        
+        if (b[0] == 0xff && b[1] == 0xd8 && b[2] == 0xff && (b[3] >> 4) == 0xe )
+        {
+            if (imptr != NULL)
+            {
+                fclose(imptr);
+            }
+
+            im_count++;
+            char im_name[20];
+            sprintf(im_name, "output/im_%d.jpeg", im_count);
+            printf("im %d\n", im_count);
+
+            imptr = fopen(im_name, "wb");
+        }
+        
+        if (imptr != NULL)
+        {
+            fwrite(b, sizeof(b), 1, imptr);
+        }
     }
 
 }
